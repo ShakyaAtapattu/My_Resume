184e7165a9c13a8c11b51ec07419a860e159ccc0
ShakyaAtapattu <74985550+ShakyaAtapattu@users.noreply.github.com>
2020-11-30 12:45:30 +0530

    Modified with CSS

--- js/main.js
+++ js/main.js
@@ -18,43 +18,19 @@ jQuery(document).ready(function( $ ) {
     interval: 3500
   });
 
-  // JavaScript Chart
+  // C Chart
   var doughnutData = [{
-      value: 70,
+      value: 90,
       color: "#1abc9c"
     },
     {
-      value: 30,
+      value: 10,
       color: "#ecf0f1"
     }
   ];
-  var myDoughnut = new Chart(document.getElementById("javascript").getContext("2d")).Doughnut(doughnutData);
+  var myDoughnut = new Chart(document.getElementById("c").getContext("2d")).Doughnut(doughnutData);
 
-  // Bootstrap Chart
-  var doughnutData = [{
-    value: 90,
-    color: "#1abc9c"
-  },
-  {
-    value: 10,
-    color: "#ecf0f1"
-  }
-  ];
-  var myDoughnut = new Chart(document.getElementById("bootstrap").getContext("2d")).Doughnut(doughnutData);
-
-  // WordPress Chart
-  var doughnutData = [{
-    value: 65,
-    color: "#1abc9c"
-  },
-  {
-    value: 35,
-    color: "#ecf0f1"
-  }
-  ];
-  var myDoughnut = new Chart(document.getElementById("wordpress").getContext("2d")).Doughnut(doughnutData);
-
-  // HTML Chart
+  // Java Chart
   var doughnutData = [{
     value: 80,
     color: "#1abc9c"
@@ -64,9 +40,9 @@ jQuery(document).ready(function( $ ) {
     color: "#ecf0f1"
   }
   ];
-  var myDoughnut = new Chart(document.getElementById("html").getContext("2d")).Doughnut(doughnutData);
+  var myDoughnut = new Chart(document.getElementById("java").getContext("2d")).Doughnut(doughnutData);
 
-  // Photoshop Chart
+  // HTML/CSS Chart
   var doughnutData = [{
     value: 70,
     color: "#1abc9c"
@@ -76,9 +52,33 @@ jQuery(document).ready(function( $ ) {
     color: "#ecf0f1"
   }
   ];
-  var myDoughnut = new Chart(document.getElementById("photoshop").getContext("2d")).Doughnut(doughnutData);
+  var myDoughnut = new Chart(document.getElementById("html").getContext("2d")).Doughnut(doughnutData);
 
-  // Illustrator Chart
+  // IntelliJ IDEA Chart
+  var doughnutData = [{
+    value: 70,
+    color: "#1abc9c"
+  },
+  {
+    value: 30,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("intelli").getContext("2d")).Doughnut(doughnutData);
+
+  // Visual Studio Chart
+  var doughnutData = [{
+    value: 70,
+    color: "#1abc9c"
+  },
+  {
+    value: 30,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("vscode").getContext("2d")).Doughnut(doughnutData);
+
+  // MYSQL Chart
   var doughnutData = [{
     value: 50,
     color: "#1abc9c"
@@ -88,5 +88,5 @@ jQuery(document).ready(function( $ ) {
     color: "#ecf0f1"
   }
   ];
-  var myDoughnut = new Chart(document.getElementById("illustrator").getContext("2d")).Doughnut(doughnutData);
+  var myDoughnut = new Chart(document.getElementById("mysql").getContext("2d")).Doughnut(doughnutData);
 });
